bb879dd5582ea466be06cc14eec753afa56f3546
Aditya Thakur <Aditya@Mac.lan>
2026-02-14 17:05:05 +0530

    Initial React project

--- src/Homefooter.jsx
+++ src/Homefooter.jsx
@@ -29,7 +29,7 @@ function Homefooter() {
     rel="noopener noreferrer"
     className="address-link"
   >
-    Pt. Deendayal Upadhyaya Computer & Informatics Centre (PDUIIC),<br/>
+    Pandit Deendayal Upadhyaya Innovation & Incubation Centre (PDUIIC),<br/>
     GJUS&T, Hisar,<br/>
     Haryana - 125001
   </a>
